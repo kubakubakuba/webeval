5babb22e18e2b33f448099a716211dcac7666235
Jakub Pelc <pelcjaku@fel.cvut.cz>
2024-02-06 16:21:14 +0100

    moved database connection into .env

--- web/db.py
+++ web/db.py
@@ -1,5 +1,16 @@
 import mysql.connector
-from db_connect import db_config
+from dotenv import load_dotenv
+import os
+
+load_dotenv("../.env")
+
+db_config = {
+	'user': os.getenv('DB_USER'),
+	'password': os.getenv('DB_PASSWORD'),
+	'host': os.getenv('DB_HOST'),
+	'database': os.getenv('DB_DATABASE'),
+	'port': os.getenv('DB_PORT')
+}
 
 def connect():
 	"""Connect to the database."""
